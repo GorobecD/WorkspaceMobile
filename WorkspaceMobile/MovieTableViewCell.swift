443db439e43757a4f758165dc46c0da58347a62c
Dima Horobets <finerlo19@gmail.com>
2020-11-20 09:05:45 -0800

    LAB3

---
+++ WorkspaceMobile/MovieTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieTableViewCell.swift
+//  WorkspaceMobile
+//
+//  Created by Dima Horobets on 08.11.2020.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
